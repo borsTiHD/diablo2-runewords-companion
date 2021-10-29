7eca75d370074f6b1ed2f69eeac0e918a26bb03b
borsTiHD <bastian.j@gmx.net>
2021-10-29 12:49:16 +0200

    ✨ App >> Adding home/index page with informations about the app.

--- pages/home.vue
+++ pages/home.vue
@@ -2,11 +2,41 @@
     <v-row justify="center" align="center">
         <v-col cols="12" sm="8" md="6">
             <v-card :elevation="getElevation" :outlined="getOutlined">
+                <v-card-title class="headline">Welcome to {{ appName }}</v-card-title>
                 <v-card-text>
-                    <v-skeleton-loader
-                        type="table-heading, paragraph, table-row-divider, sentences, button, table-tfoot"
-                    />
+                    <p>
+                        {{ appName }} is a small companion App for Diablo 2 Resurrected.
+                        It was designed to help with building runewords, as well as get information about all runes.
+                    </p>
+                    <p>
+                        For more information on {{ appName }}, check out the
+                        <a :href="repoUrl" target="_blank" rel="noopener noreferrer">github repository</a>.
+                    </p>
+                    <p>
+                        Found a bug or have a question? Report it on the github
+                        <a
+                            :href="`${repoUrl}/issues`"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="contribute"
+                        >issue board</a>.
+                    </p>
+                    <p>
+                        Thank you for trying {{ appName }} and I look forward to bringing more exciting features in
+                        the future.
+                    </p>
+                    <div class="text-xs-right">
+                        <em><small>&mdash; {{ appAuthor }}</small></em>
+                    </div>
+                    <hr class="my-3">
+                    <a :href="repoUrl" target="_blank" rel="noopener noreferrer">
+                        {{ appName }} GitHub
+                    </a>
                 </v-card-text>
+                <v-card-actions>
+                    <v-spacer />
+                    <v-btn color="primary" nuxt :to="continueRoute">Continue</v-btn>
+                </v-card-actions>
             </v-card>
         </v-col>
     </v-row>
@@ -14,14 +44,35 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import pkg from 'projRoot/package.json'
 
 export default {
     name: 'Home',
+    auth: false,
+    data() {
+        return {
+            continueRoute: '/runes'
+        }
+    },
+    head() {
+        return {
+            title: `${this.$options.name} | ${this.headTitle()}`
+        }
+    },
     computed: {
         ...mapGetters({
             getElevation: 'settings/getElevation',
             getOutlined: 'settings/getOutlined'
-        })
+        }),
+        repoUrl() {
+            return pkg.repository.url
+        },
+        appName() {
+            return pkg.productName
+        },
+        appAuthor() {
+            return pkg.author
+        }
     }
 }
 </script>
