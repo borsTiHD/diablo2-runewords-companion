f858cc7fa5b6ddd000d81aaf86ff5ee611205b2e
borsTiHD <bastian.j@gmx.net>
2021-11-03 22:52:51 +0100

    ✨ RuneInventory >> Preperations for setting inventory.

---
+++ components/runewords/RuneInventory.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card :elevation="getElevation" :outlined="getOutlined">
+        <v-card-title>
+            <v-row>
+                <v-col v-for="(rune, index) in getRuneList" :key="index" cols="auto">
+                    <v-card elevation="4">
+                        {{ rune.name }}
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-card-title>
+    </v-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'RuneInventory',
+    computed: {
+        ...mapGetters({
+            getElevation: 'settings/getElevation',
+            getOutlined: 'settings/getOutlined',
+            getRuneList: 'runes/getRuneList'
+        })
+    }
+}
+</script>
--- pages/runewords.vue
+++ pages/runewords.vue
@@ -1,20 +1,29 @@
 <template>
-    <v-row justify="center">
-        <template v-for="(runeword, index) in getRunewordList">
-            <v-col :key="index" cols="12" sm="8" md="6" lg="4" xl="3" class="ma-4 d-flex flex-column">
-                <runeword-display :runeword="runeword" />
-            </v-col>
-        </template>
+    <v-row>
+        <v-col cols="12">
+            <rune-inventory />
+        </v-col>
+        <v-col cols="12">
+            <v-row justify="center">
+                <template v-for="(runeword, index) in getRunewordList">
+                    <v-col :key="index" cols="12" sm="8" md="6" lg="4" xl="3" class="ma-4 d-flex flex-column">
+                        <runeword-display :runeword="runeword" />
+                    </v-col>
+                </template>
+            </v-row>
+        </v-col>
     </v-row>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import RuneInventory from '~/components/runewords/RuneInventory.vue'
 import RunewordDisplay from '~/components/display/RunewordDisplay.vue'
 
 export default {
     name: 'RunewordsPage',
     components: {
+        RuneInventory,
         RunewordDisplay
     },
     head() {
