1c429dd7f825d23c88bb33f1b850842573d71069
borsTiHD <bastian.j@gmx.net>
2021-11-04 16:26:12 +0100

    🎨 Runewords >> Improving the way, how renderes runewords are shown. All runewords are renderes, but will be invisible if it shouldn't be displayed.

--- pages/runewords.vue
+++ pages/runewords.vue
@@ -5,8 +5,6 @@
                 <v-card-actions>
                     <v-btn
                         block
-                        :disabled="deleteBtnLoading"
-                        :loading="deleteBtnLoading"
                         @click="deleteAll"
                     >
                         Delete All
@@ -19,11 +17,9 @@
         </v-col>
         <v-col cols="10" sm="12" md="9" lg="10">
             <v-row justify="center">
-                <template v-for="(runeword, index) in runewordList">
-                    <v-col :key="index" cols="12" sm="8" md="8" lg="4" xl="3" class="mx-2 d-flex flex-column">
-                        <runeword-display :runeword="runeword" />
-                    </v-col>
-                </template>
+                <v-col v-for="(runeword, index) in getRunewordList" :key="index" :class="showRuneword(runeword.name) ? '' : 'hide'" cols="12" sm="8" md="8" lg="4" xl="3" class="mx-2 d-flex flex-column">
+                    <runeword-display :runeword="runeword" />
+                </v-col>
             </v-row>
         </v-col>
     </v-row>
@@ -40,11 +36,6 @@ export default {
         RuneInventory,
         RunewordDisplay
     },
-    data() {
-        return {
-            deleteBtnLoading: false
-        }
-    },
     head() {
         return {
             title: `${this.$options.name} | ${this.headTitle()}`
@@ -58,14 +49,16 @@ export default {
             getRunewordList: 'runewords/getRunewordList',
             getRuneList: 'runes/getRuneList'
         }),
-        runewordList() {
-            // Reducing stock and calculates how many items the user has
+        calculatedRunewordList() {
+            // Reducing item stock and calculates how many items the user has
+            // Returns array of runeword names, that can be build with the stock
             // https://stackoverflow.com/a/6300596/7625095
             const itemsInStock = this.getInventory.reduce((acc, obj) => { return acc + obj.value }, 0)
             if (itemsInStock <= 0) {
-                return this.getRunewordList
+                // Returning complete list with names, if the user dont have any item in stock
+                return this.getRunewordList.map((runeword) => runeword.name)
             }
-            return []
+            return ['Insight', 'Holy Thunder']
         }
     },
     methods: {
@@ -73,16 +66,19 @@ export default {
             clearInventory: 'inventory/clearInventory'
         }),
         async deleteAll() {
-            this.deleteBtnLoading = true
-            // Workaround, so the see a loading animation/disabling the button
-            setTimeout(() => {
-                this.clearInventory()
-                this.deleteBtnLoading = false
-            }, 100)
+            this.clearInventory()
+        },
+        showRuneword(name) {
+            const check = this.calculatedRunewordList.includes(name)
+            return check
         }
     }
 }
 </script>
 
 <style scoped>
+/* Workaround -> 'v-show' is not working because it misses the '!important' */
+.hide {
+    display: none !important;
+}
 </style>
